799f674695ef5d4538b6bf43d4e38554f1a6f62c
Mathies Wedler <mathies.wedler@dlr.de>
2025-07-25 16:28:59 +0200

    initial commit

---
+++ src/keras_addon/layers/__init__.py
@@ -0,0 +1,5 @@
+from .fno import SpectralConv1D, SpectralConv2D
+
+from .merge import AttentionGate
+
+from .wrapper import SpatialAttention, ChannelAttention, CBAM, FiLM, Residual, TemporalSlice, WeightNormalization
