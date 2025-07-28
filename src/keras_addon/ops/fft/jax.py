07226ac321b77ad29f80b0999981b8c905838726
Mathies Wedler <mathies.wedler@dlr.de>
2025-07-28 10:27:40 +0200

    add 3D fft operations

--- src/keras_addon/ops/fft/jax.py
+++ src/keras_addon/ops/fft/jax.py
@@ -5,15 +5,32 @@ from functools import partial
 
 
 def _get_complex_tensor_from_tuple(x) -> jax.lax.complex:
+    """
+    Get a complex tensor from a tuple of two real-valued tensors.
+
+    Parameters
+    ----------
+    x : jax.lax.real | list | tuple
+        Input tensor, either `jax.lax.real` (real part) or tuple/list (real- and imaginary part).
+        If only the real part is provided, the imaginary part is assumed 0.
+
+    Returns
+    -------
+    y : jax.lax.complex
+        Tensor with `dtype=jax.lax.complex`.
+
+    Raises
+    ------
+    ValueError
+        If shapes of real- and imaginary part to not match.
+    ValueError
+        If at least on of the inputs is not of type float.
+
+    """
+
     if not isinstance(x, (tuple, list)) or len(x) != 2:
         real = x
         imag = jnp.zeros_like(x)
-        # raise ValueError(
-        #     "Input `x` should be a tuple of two tensors - real and imaginary."
-        #     f"Received: x={x}"
-        # )
-    # `convert_to_tensor` does not support passing complex tensors. We separate
-    # the input out into real and imaginary and convert them separately.
     else:
         real, imag = x
     # Check shapes.
@@ -31,32 +48,9 @@ def _get_complex_tensor_from_tuple(x) -> jax.lax.complex:
             "At least one tensor in input `x` is not of type float."
             f"Received: x={x}."
         )
+    
     complex_input = jax.lax.complex(real, imag)
     return complex_input
-
-
-def _get_real_tensor_from_tuple(x) -> jax.lax.real:
-    if not isinstance(x, (tuple, list)) or len(x) != 2:
-        return x
-    # `convert_to_tensor` does not support passing complex tensors. We separate
-    # the input out into real and imaginary and convert them separately.
-    real, imag = x
-    # Check shapes.
-    if real.shape != imag.shape:
-        raise ValueError(
-            "Input `x` should be a tuple of two tensors - real and imaginary."
-            "Both the real and imaginary parts should have the same shape. "
-            f"Received: x[0].shape = {real.shape}, x[1].shape = {imag.shape}"
-        )
-    # Ensure dtype is float.
-    if not jnp.issubdtype(real.dtype, jnp.floating) or not jnp.issubdtype(
-        imag.dtype, jnp.floating
-    ):
-        raise ValueError(
-            "At least one tensor in input `x` is not of type float."
-            f"Received: x={x}."
-        )
-    return real  # NOTE return only real valued output, neglect imaginary part
 
 
 # === FFT ===
@@ -111,6 +105,10 @@ def rfft2_fn(x):
     return partial(_rfft, fn=jnp.fft.rfft2)(x)
 
 
+def ifft3_fn(x):
+    return partial(_fft, fn=jnp.fft.rfftn, axes=(-3, -2, -1))(x)
+
+
 def irfft_fn(x, n: tuple = None):
     if isinstance(n, tuple):
         n, = n  # unpack tuple
@@ -120,4 +118,9 @@ def irfft_fn(x, n: tuple = None):
 
 def irfft2_fn(x, n: tuple = None):
     y_real, _ = partial(_irfft, fn=jnp.fft.irfft2, s=n)(x)
+    return y_real
+
+
+def irfft2_fn(x, n: tuple = None):
+    y_real, _ = partial(_irfft, fn=jnp.fft.irfftn, s=n, axes=(-3, -2, -1))(x)
     return y_real
--- src/keras_addon/ops/fft/tensorflow.py
+++ src/keras_addon/ops/fft/tensorflow.py
@@ -3,23 +3,43 @@ from typing import Tuple
 from functools import partial
 
 
-def _get_complex_tensor_from_tuple(x) -> tf.Tensor:
+def _get_complex_tensor_from_tuple(x):
+    """
+    Get a complex tensor from a tuple of two real-valued tensors.
+
+    Parameters
+    ----------
+    x : tf.Tensor | list | tuple
+        Input tensor, either `tf.Tensor` (real part) or tuple/list (real- and imaginary part).
+        If only the real part is provided, the imaginary part is assumed 0.
+
+    Returns
+    -------
+    y : tf.Tensor
+        Tensor with `dtype=tf.complex`.
+
+    Raises
+    ------
+    ValueError
+        If shapes of real- and imaginary part to not match.
+
+    """
+
     if not isinstance(x, (tuple, list)) or len(x) != 2:
         real = x
         imag = tf.zeros_like(x)
     else:
         real, imag = x
+    # Check shapes.
+    if real.shape != imag.shape:
+        raise ValueError(
+            "Input `x` should be a tuple of two tensors - real and imaginary."
+            "Both the real and imaginary parts should have the same shape. "
+            f"Received: x[0].shape = {real.shape}, x[1].shape = {imag.shape}"
+        )
 
     complex_input = tf.complex(real=real, imag=imag)
     return complex_input
-
-
-def _get_real_tensor_from_tuple(x) -> tf.Tensor:
-    if not isinstance(x, (tuple, list)) or len(x) != 2:
-        return x
-    
-    real, _ = x
-    return real
 
 
 # === FFT ===
@@ -49,12 +69,20 @@ def fft2_fn(x):
     return partial(_fft, fn=tf.signal.fft2d)(x)
 
 
+def fft3_fn(x):
+    return partial(_fft, fn=tf.signal.fft3d)(x)
+
+
 def ifft_fn(x):
     return partial(_fft, fn=tf.signal.ifft)(x)
 
 
 def ifft2_fn(x):
     return partial(_fft, fn=tf.signal.ifft2d)(x)
+
+
+def ifft3_fn(x):
+    return partial(_fft, fn=tf.signal.ifft3d)(x)
 
 
 def rfft_fn(x):
@@ -65,6 +93,10 @@ def rfft2_fn(x):
     return partial(_rfft, fn=tf.signal.rfft2d)(x)
 
 
+def rfft3_fn(x):
+    return partial(_rfft, fn=tf.signal.rfft3d)(x)
+
+
 def irfft_fn(x, n: tuple = None):
     y_real, _ = partial(_irfft, fn=tf.signal.irfft, n=n)(x)
     return y_real
@@ -72,4 +104,9 @@ def irfft_fn(x, n: tuple = None):
 
 def irfft2_fn(x, n: tuple = None):
     y_real, _ = partial(_irfft, fn=tf.signal.irfft2d, n=n)(x)
+    return y_real
+
+
+def irfft3_fn(x, n: tuple = None):
+    y_real, _ = partial(_irfft, fn=tf.signal.irfft3d, n=n)(x)
     return y_real
