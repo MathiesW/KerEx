43ca295bce943dceab06534911094d79208126d3
Mathies Wedler <mathies.wedler@dlr.de>
2025-09-10 16:23:58 +0200

    add imports for `SmoothUnet`

--- src/kerex/models/__init__.py
+++ src/kerex/models/__init__.py
@@ -1,5 +1,5 @@
 from. autoencoder import FCN1D, FCN2D, FCN3D
-from .autoencoder import Unet1D, Unet2D, Unet3D
+from .autoencoder import Unet1D, Unet2D, Unet3D, SmoothUnet1D, SmoothUnet2D, SmoothUnet3D
 
 from .neural_operator import NeuralOperator1D, NeuralOperator2D
 
