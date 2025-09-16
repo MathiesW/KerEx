a62bc9f20993fe61c0715b7034ae4752f3f85586
Mathies Wedler <mathies.wedler@dlr.de>
2025-09-16 09:46:22 +0200

    add FFT Upsampling

--- src/kerex/layers/__init__.py
+++ src/kerex/layers/__init__.py
@@ -4,6 +4,6 @@ from .merge import AttentionGate
 
 from .pooling import ChannelAttentionModule1D, ChannelAttentionModule2D, ChannelAttentionModule3D
 
-from .reshape import SmoothUpSampling1D, SmoothUpSampling2D, SmoothUpSampling3D
+from .reshape import SmoothUpSampling1D, SmoothUpSampling2D, SmoothUpSampling3D, FFTUpSampling1D, FFTUpSampling2D, FFTUpSampling3D
 
 from .wrapper import SpatialAttention, ChannelAttention, CBAM, FiLM, Residual, TemporalSlice, WeightNormalization
--- src/kerex/layers/reshape/__init__.py
+++ src/kerex/layers/reshape/__init__.py
@@ -1,1 +1,2 @@
-from .upsampling import SmoothUpSampling1D, SmoothUpSampling2D, SmoothUpSampling3D
+from .smooth_upsampling import SmoothUpSampling1D, SmoothUpSampling2D, SmoothUpSampling3D
+from .fft_upsampling import FFTUpSampling1D, FFTUpSampling2D, FFTUpSampling3D
