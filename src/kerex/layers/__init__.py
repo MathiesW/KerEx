c650ec89ac0146b0de8cad526f1461e3617b6e19
Mathies Wedler <mathies.wedler@dlr.de>
2025-09-24 09:36:24 +0200

    update to v1.1.3

--- src/kerex/layers/__init__.py
+++ src/kerex/layers/__init__.py
@@ -2,7 +2,7 @@ from .fno import FNO1D, FNO2D, FNO3D
 
 from .merge import AttentionGate
 
-from .pooling import ChannelAttentionModule1D, ChannelAttentionModule2D, ChannelAttentionModule3D
+from .pooling import ChannelAttentionModule1D, ChannelAttentionModule2D, ChannelAttentionModule3D, AttentionPooling1D, AttentionPooling2D, AttentionPooling3D
 
 from .reshape import SmoothUpSampling1D, SmoothUpSampling2D, SmoothUpSampling3D, FFTUpSampling1D, FFTUpSampling2D, FFTUpSampling3D
 
